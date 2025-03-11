1d276fb05a72a2c44c8af496dcccbbd44af1e265
Ina Jobbet <ina.eklund@ljm.se>
2025-03-11 10:59:06 +0100

    name alias function added

---
+++ src/components/mptab-alias.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import { BaseControl, ToggleControl, TextControl } from '@wordpress/components'
+import { __ } from '@wordpress/i18n'
+interface Props {
+	element: {
+		id: string
+		label: string
+		value: string
+		input: HTMLInputElement
+	}[]
+}
+export function MPTABAlias({ element }: Props) {
+	const [values, setValues] = useState(element.map((element) => element.value))
+
+	const [useAlias, setUseAlias] = useState(
+		element.some((element) => element.value.length > 0)
+	)
+
+	const inputs = element.map((element, index) => {
+		return (
+			<>
+				<TextControl
+					label={element.label}
+					value={values[index]}
+					onChange={(value) => {
+						setValues((oldValues) => {
+							const newValues = oldValues.concat()
+							newValues[index] = value
+							return newValues
+						})
+						element.input.value = value
+					}}
+				/>
+			</>
+		)
+	})
+	return (
+		<BaseControl
+			label={__('Name Alias', 'mptab-domain')}
+			help={__('Ex. october, autumn or 3:rd quarter', 'mptab-domain')}
+		>
+			<ToggleControl
+				label={__('Use alias', 'mptab-domain')}
+				checked={useAlias}
+				onChange={() => {
+					setUseAlias((state) => {
+						const newState = !state
+						if (!newState) {
+							setValues(['', ''])
+							element[0].input.value = ''
+							element[1].input.value = ''
+						}
+						return newState
+					})
+				}}
+			/>
+			{useAlias ? inputs : ''}
+		</BaseControl>
+	)
+}
--- src/exhibition_meta.tsx
+++ src/exhibition_meta.tsx
@@ -2,6 +2,8 @@ import domReady from '@wordpress/dom-ready'
 import { createRoot } from '@wordpress/element'
 
 import { MPTABDateRange } from './components/mptab-date-range'
+import { MPTABAlias } from './components/mptab-alias'
+import { __ } from '@wordpress/i18n'
 
 domReady(() => {
 	const root = createRoot(
@@ -22,11 +24,41 @@ domReady(() => {
 	const minDate = parseInt(mindateInput.value)
 	const maxDate = parseInt(maxdateInput.value)
 
+	//alias
+	const startAliasInput: HTMLInputElement | null = document.querySelector(
+		'#mptab-exhibition-date-start-alias-field'
+	)
+	const endAliasInput: HTMLInputElement | null = document.querySelector(
+		'#mptab-exhibition-date-end-alias-field'
+	)
+	if (!startAliasInput || !endAliasInput) {
+		throw new Error('alias_field not found')
+	}
+
+	const startAlias = startAliasInput.value
+	const endAlias = endAliasInput.value
+
 	root.render(
 		<>
 			<MPTABDateRange
 				dates={[minDate, maxDate]}
 				input={[mindateInput, maxdateInput]}
+			/>
+			<MPTABAlias
+				element={[
+					{
+						id: 'mptab-exhibition-date-start-alias',
+						label: __('Start alias', 'mptab-domain'),
+						value: startAlias,
+						input: startAliasInput,
+					},
+					{
+						id: 'mptab-exhibition-date-end-alias',
+						label: __('End alias', 'mptab-domain'),
+						value: endAlias,
+						input: endAliasInput,
+					},
+				]}
 			/>
 		</>
 	)
