6f62ffcb53306e86c4e9776daa2411b78a382042
Ina Jobbet <ina.eklund@ljm.se>
2025-03-17 12:09:05 +0100

    bug fix: in event date component show already selected dates

--- src/input/mptab-event-date.tsx
+++ src/input/mptab-event-date.tsx
@@ -1,15 +1,29 @@
 import { MPTABDateSelect } from '../components/mptab-date-select'
 import { MPTABAlias } from '../components/mptab-alias'
 
+import { isJSON } from '../utility/is-JSON'
+
+import { DatesSchema } from '../types/mptab-date-types'
+
 interface Props {
 	datesInput: HTMLInputElement[]
 	aliasInput: HTMLInputElement[]
 }
 
 export function MPTABEventDate({ datesInput, aliasInput }: Props) {
+	if (!datesInput[0]) {
+		throw new Error('mptab-event-time-field not found')
+	}
+	let dates = []
+	if (
+		isJSON(datesInput[0].value) &&
+		DatesSchema.safeParse(JSON.parse(datesInput[0].value))
+	) {
+		dates = JSON.parse(datesInput[0].value)
+	}
 	return (
 		<>
-			<MPTABDateSelect dates={[]} input={datesInput} />
+			<MPTABDateSelect dates={dates} input={datesInput} />
 			<MPTABAlias
 				element={[
 					{
