b5bab37424e4b880874e74ec42f3c0a228eed1e7
Ina Jobbet <ina.eklund@ljm.se>
2025-03-24 11:41:21 +0100

    added bootstrap and change event img to background insted of img tag

--- src/components/mptab-event.tsx
+++ src/components/mptab-event.tsx
@@ -7,11 +7,12 @@ interface Props {
 export function MPTABEvent({ item }: Props) {
 	const img = item.thumbnail && item.thumbnail != true ? item.thumbnail : 'test'
 	return (
-		<div className='mptab_event mptab_event_display mptab_event_display_min'>
-			<div className='mptab_event mptab_event_display_img'>
-				<img src={img} />
-			</div>
-			<div>
+		<div className='mptab_event mptab_event_display mptab_event_display_min row'>
+			<div
+				className='mptab_event mptab_event_display_img col'
+				style={{ backgroundImage: `url(${img})` }}
+			></div>
+			<div className='col'>
 				<h3>{item.title}</h3>
 
 				<p>{item.exerpt}</p>
