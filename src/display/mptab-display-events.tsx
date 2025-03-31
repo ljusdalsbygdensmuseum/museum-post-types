c4fc730d1369a9d50433fcf6855153393e87d4ba
Ina Jobbet <ina.eklund@ljm.se>
2025-03-31 14:01:40 +0200

    added comming events and exhibs, added titles, sass: every other is flipped

--- src/display/mptab-display-events.tsx
+++ src/display/mptab-display-events.tsx
@@ -1,5 +1,6 @@
 import apiFetch from '@wordpress/api-fetch'
 import { useState, useEffect } from 'react'
+import { __ } from '@wordpress/i18n'
 
 import { MPTABEvent } from '../components/mptab-event'
 
@@ -33,5 +34,15 @@ export function MPTABDisplayEvent({ path }: Props) {
 	const current = data.current.map((item) => {
 		return <MPTABEvent item={item} />
 	})
-	return current
+	const comming = data.comming.map((item) => {
+		return <MPTABEvent item={item} />
+	})
+	return (
+		<section>
+			<h2>{__('Right now', 'mptab-domain')}</h2>
+			{current}
+			<h2>{__('Coming', 'mptab-domain')}</h2>
+			{comming}
+		</section>
+	)
 }
