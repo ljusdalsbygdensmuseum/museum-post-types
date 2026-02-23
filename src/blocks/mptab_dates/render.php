c1fecb56d23c18a4d2eedd5086ef6bab897709e9
Ina Jobbet <ina.eklund@ljm.se>
2026-02-23 11:59:05 +0100

    end of day commit, figuring out how to format dates

--- src/blocks/mptab_dates/render.php
+++ src/blocks/mptab_dates/render.php
@@ -7,6 +7,21 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+$data;
+
+if (get_post_type() == 'mptab_exhibition') {
+    $start = get_post_meta(get_the_ID(), 'mptab-exhibition-date-start');
+    $end = get_post_meta(get_the_ID(), 'mptab-exhibition-date-end');
+    $aliasStart = get_post_meta(get_the_ID(), 'mptab-exhibition-date-start-alias');
+    $aliasEnd = get_post_meta(get_the_ID(), 'mptab-exhibition-date-end-alias');
+
+    $data = 'data-dates="[{date:"' . json_encode($start) . '"}, {date:"' . json_encode($end) . '"}]" data-alias="[' . $aliasStart . ', ' . $aliasEnd . ']"';
+} elseif ((get_post_type() == 'mptab_event')) {
+    $dates = get_post_meta(get_the_ID(), 'mptab-event-date-start');
+    $alias = get_post_meta(get_the_ID(), 'mptab-event-date-alias');
+
+    $data = 'data-dates="' . json_encode($dates) . '" data-alias="' . json_encode($alias) . '"';
+}
 ?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
+<div <?php echo get_block_wrapper_attributes(); ?> data-post-type="<?php echo get_post_type(); ?>" <?php echo $data; ?>>
 </div>
--- src/blocks/mptab_dates/view.tsx
+++ src/blocks/mptab_dates/view.tsx
@@ -1,9 +1,17 @@
 import domReady from '@wordpress/dom-ready'
 import { createRoot } from '@wordpress/element'
+import { DatesSchema } from '../../../src/types/mptab-date-types'
 
 domReady(() => {
 	const elements = document.querySelectorAll('.wp-block-mptab-ex-ev-dates')
-	elements.forEach((element) => {
+	//@ts-ignore
+	elements.forEach((element: HTMLDivElement) => {
+		if (DatesSchema.safeParse(element.dataset.dates).success) {
+			console.log(DatesSchema.parse(element.dataset.dates))
+		} else {
+			console.log(DatesSchema.safeParse(element.dataset.dates))
+		}
+
 		const root = createRoot(element!)
 		root.render(
 			<>
