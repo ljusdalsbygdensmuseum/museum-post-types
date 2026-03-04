e1d6146bda77a7a7e3d3e288df3aa69cfb6f1c53
Ina Eklund <theartflower@gmail.com>
2026-03-04 11:14:26 +0100

    display dates

--- src/display/mptab-display-dates.tsx
+++ src/display/mptab-display-dates.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import apiFetch from '@wordpress/api-fetch'
 import { MPTABDate } from '../../src/components/mptab-date'
+import { WP_REST_API_Exhib } from '../../src/types/mptab-rest-types'
+import { Dates } from '../../src/types/mptab-date-types'
 
 interface Props {
 	route: string
 }
 
 const MptabDisplayDates = ({ route }: Props) => {
-	return <div></div>
+	const [dates, setDates] = useState<Dates>()
+	const [alias, setAlias] = useState<string[]>([])
+	const [postType, setPostType] = useState('')
+
+	useEffect(() => {
+		apiFetch<WP_REST_API_Exhib>({ path: route }).then(
+			(data: WP_REST_API_Exhib) => {
+				setPostType(data.type)
+				if (!data.mptab_date.dates) {
+					return
+				}
+				setDates(data.mptab_date.dates)
+				if (data.mptab_date.alias) {
+					setAlias(data.mptab_date.alias)
+				}
+			},
+		)
+	}, [])
+
+	return <MPTABDate dates={dates} alias={alias} post_type={postType} />
 }
 
 export default MptabDisplayDates
