e053e7420020da567e5ec7f8f0c183536d27e146
Ina Eklund <theartflower@gmail.com>
2026-06-24 13:20:02 +0200

    postcss file caused an eternal webpack loop

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	plugins: [
-		require('@tailwindcss/postcss'),
-		require('autoprefixer')({ grid: true }),
-		...(process.env.NODE_ENV === 'production' ? require('cssnano') : ''),
-	],
-}
