19d5193210a6fd03c02d3e1a497a86774cb150ca
Ina Eklund <theartflower@gmail.com>
2026-08-12 11:59:44 +0200

    now sorts by order numb

--- index.php
+++ index.php
@@ -595,6 +595,9 @@ class PluginBoilerplate
         $servicesQuery = new WP_Query(array(
             'post_type' => 'mptab_service',
             'posts_per_page' => -1,
+            'orderby' => 'meta_value_number',
+            'order' => 'ASC',
+            'meta_key' => 'mptab-service_order'
         ));
 
         $posts = [];
