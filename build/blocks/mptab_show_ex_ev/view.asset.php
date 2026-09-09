17ab29a6317b1967705bdf991fca0017cfbd9c20
Ina Eklund <theartflower@gmail.com>
2026-09-09 11:11:35 +0200

    animated show ex ev titles

--- build/blocks/mptab_show_ex_ev/view.asset.php
+++ build/blocks/mptab_show_ex_ev/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2eaaafd4b0cf962049b4');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '47420049c91074c7db2f');
--- src/display/mptab-display-events.tsx
+++ src/display/mptab-display-events.tsx
@@ -1,6 +1,7 @@
 import apiFetch from '@wordpress/api-fetch'
 import { useState, useEffect } from 'react'
 import { __ } from '@wordpress/i18n'
+import { motion } from 'motion/react'
 
 import { MPTABEvent } from '../components/mptab-event'
 
@@ -37,11 +38,25 @@ export function MPTABDisplayEvent({ path }: Props) {
 	const comming = data.comming.map((item) => {
 		return <MPTABEvent item={item} />
 	})
+
+	//motion variants
+	const motionHeading = {
+		initial: { opacity: 0, y: 15 },
+		visible: { opacity: 1, y: 0 },
+	}
+
 	return (
 		<section>
 			{current.length ? (
 				<div className='mptab_event_display_current'>
-					<h2>{__('Right now', 'mptab-domain')}</h2>
+					<motion.h2
+						variants={motionHeading}
+						initial={'initial'}
+						whileInView={'visible'}
+						viewport={{ once: true, margin: '-20px' }}
+					>
+						{__('Right now', 'mptab-domain')}
+					</motion.h2>
 					{current}
 				</div>
 			) : (
@@ -49,7 +64,14 @@ export function MPTABDisplayEvent({ path }: Props) {
 			)}
 			{comming.length ? (
 				<div className='mptab_event_display_comming'>
-					<h2>{__('Coming', 'mptab-domain')}</h2>
+					<motion.h2
+						variants={motionHeading}
+						initial={'initial'}
+						whileInView={'visible'}
+						viewport={{ once: true, margin: '-20px' }}
+					>
+						{__('Coming', 'mptab-domain')}
+					</motion.h2>
 					{comming}
 				</div>
 			) : (
