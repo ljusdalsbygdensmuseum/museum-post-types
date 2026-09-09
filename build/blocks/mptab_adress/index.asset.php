57bc61becc261b8d79424dcb4b10d031325c51be
Ina Eklund <theartflower@gmail.com>
2026-09-09 10:37:36 +0200

    added bg animation to skeleton

--- build/blocks/mptab_adress/index.asset.php
+++ build/blocks/mptab_adress/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks'), 'version' => '65444d65a22e828a97b8');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks'), 'version' => 'c30fb81674178214eed0');
