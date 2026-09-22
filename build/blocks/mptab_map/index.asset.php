a96eb0f929a95295a96bc2ac7390570b941c34f7
Ina Eklund <theartflower@gmail.com>
2026-09-22 10:39:12 +0200

    map skeleton animation

--- build/blocks/mptab_map/index.asset.php
+++ build/blocks/mptab_map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks'), 'version' => '34d5016e3d466ac7535b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks'), 'version' => '097cd19293ed2aeba934');
